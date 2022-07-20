75125e10d0565ca63b696a0d538d1156938560f2
PawZet <pawelzurawski898@gmail.com>
2022-07-20 23:04:57 +0200

    reaction timer app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Reaction Timer</h1>
+  <h3>Check your reaction time!</h3>
+  <button @click="start" :disabled="isPlaying">PLAY</button>
+  <BlockComponent v-if="isPlaying" :delay="delay" @end="endGame" />
+  <ResultsComponent v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import BlockComponent from "./components/BlockComponent.vue";
+import ResultsComponent from "./components/ResultsComponent.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { BlockComponent, ResultsComponent },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 3000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
   },
 };
 </script>
@@ -20,7 +42,21 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #194878;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/ResultsComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: "",
+    };
+  },
+  mounted() {
+    if (this.score < 250) return (this.rank = "Ninja fingers!");
+    if (this.score < 500) return (this.rank = "Rapid reflexes!");
+    else return (this.rank = "Snail pace...!");
+  },
+};
+</script>
+
+<style>
+.rank {
+  color: #0fe087;
+  font-weight: bold;
+}
+</style>
